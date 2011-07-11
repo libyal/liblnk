08bf353543b2413b7863f5f24b4a7510d0e5d3e5
Joachim Metz <joachim.metz@gmail.com>
2011-07-11 21:44:36 +0200

    Changes of version: 20110711

--- liblnk/liblnk_distributed_link_tracker_properties.h
+++ liblnk/liblnk_distributed_link_tracker_properties.h
@@ -19,8 +19,8 @@
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined( _LIBLNK_DISTRIBUTED_LINK_TRACKER_H )
-#define _LIBLNK_DISTRIBUTED_LINK_TRACKER_H
+#if !defined( _LIBLNK_DISTRIBUTED_LINK_TRACKER_PROPERTIES_H )
+#define _LIBLNK_DISTRIBUTED_LINK_TRACKER_PROPERTIES_H
 
 #include <common.h>
 #include <types.h>
