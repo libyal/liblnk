1b987d947e877cee19927f65136054bf05dfd0ba
Joachim Metz <joachim.metz@gmail.com>
2013-09-23 07:14:09 +0200

    Changes of version: 20130923

--- liblnk/liblnk_libuna.h
+++ liblnk/liblnk_libuna.h
@@ -32,8 +32,8 @@
 #include <libuna_base32_stream.h>
 #include <libuna_base64_stream.h>
 #include <libuna_byte_stream.h>
-#include <libuna_error.h>
 #include <libuna_unicode_character.h>
+#include <libuna_url_stream.h>
 #include <libuna_utf8_stream.h>
 #include <libuna_utf8_string.h>
 #include <libuna_utf16_stream.h>
--- lnktools/lnktools_libfdatetime.h
+++ lnktools/lnktools_libfdatetime.h
@@ -32,6 +32,7 @@
 #include <libfdatetime_definitions.h>
 #include <libfdatetime_fat_date_time.h>
 #include <libfdatetime_filetime.h>
+#include <libfdatetime_floatingtime.h>
 #include <libfdatetime_nsf_timedate.h>
 #include <libfdatetime_posix_time.h>
 #include <libfdatetime_systemtime.h>
