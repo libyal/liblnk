0fd5355548b5be9b1b06b915d681ef53d1477dcc
Joachim Metz <joachim.metz@gmail.com>
2014-01-12 15:44:58 +0100

    Changes of version: 20140112

--- pylnk/pylnk_file.h
+++ pylnk/pylnk_file.h
@@ -1,7 +1,7 @@
 /*
  * Python object definition of the liblnk file
  *
- * Copyright (c) 2009-2013, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (c) 2009-2014, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
