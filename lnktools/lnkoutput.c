07046af2963574b03ee79c201b28c2256477faf5
Joachim Metz <joachim.metz@gmail.com>
2016-01-07 06:30:27 +0100

    Applied updates.

--- lnktools/lnkoutput.c
+++ lnktools/lnkoutput.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the lnktools
  *
- * Copyright (C) 2009-2015, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -46,7 +46,7 @@ void lnkoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2009-2015, %s.\n" ),
+	 _( "Copyright (C) 2009-2016, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
