3283cfa55fb8c29d42ca78fa191690cfc43f2fae
Joachim Metz <joachim.metz@gmail.com>
2016-09-25 06:16:35 +0200

    Worked on tests and Windows test scripts.

--- tests/lnk_test_memory.c
+++ tests/lnk_test_memory.c
@@ -55,8 +55,8 @@ void *malloc(
 	if( lnk_test_real_malloc == NULL )
 	{
 		lnk_test_real_malloc = dlsym(
-		                           RTLD_NEXT,
-		                           "malloc" );
+		                        RTLD_NEXT,
+		                        "malloc" );
 	}
 	if( lnk_test_malloc_attempts_before_fail == 0 )
 	{
@@ -86,8 +86,8 @@ void *memcpy(
 	if( lnk_test_real_memcpy == NULL )
 	{
 		lnk_test_real_memcpy = dlsym(
-		                           RTLD_NEXT,
-		                           "memcpy" );
+		                        RTLD_NEXT,
+		                        "memcpy" );
 	}
 	if( lnk_test_memcpy_attempts_before_fail == 0 )
 	{
@@ -119,8 +119,8 @@ void *memset(
 	if( lnk_test_real_memset == NULL )
 	{
 		lnk_test_real_memset = dlsym(
-		                           RTLD_NEXT,
-		                           "memset" );
+		                        RTLD_NEXT,
+		                        "memset" );
 	}
 	if( lnk_test_memset_attempts_before_fail == 0 )
 	{
@@ -151,8 +151,8 @@ void *realloc(
 	if( lnk_test_real_realloc == NULL )
 	{
 		lnk_test_real_realloc = dlsym(
-		                            RTLD_NEXT,
-		                            "realloc" );
+		                         RTLD_NEXT,
+		                         "realloc" );
 	}
 	if( lnk_test_realloc_attempts_before_fail == 0 )
 	{
