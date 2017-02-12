3e60bb1d05a129055aa374fb33682cfb0dfcd66e
Joachim Metz <joachim.metz@gmail.com>
2017-02-12 13:00:34 +0100

    Worked on tests and applied updates

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,6 +1,6 @@
-# Library API type testing script
+# Tests C library functions and types.
 #
-# Version: 20161110
+# Version: 20170115
 
 $ExitSuccess = 0
 $ExitFailure = 1
@@ -10,17 +10,17 @@ $TestPrefix = Split-Path -path ${Pwd}.Path -parent
 $TestPrefix = Split-Path -path ${TestPrefix} -leaf
 $TestPrefix = ${TestPrefix}.Substring(3)
 
-$TestTypes = "data_block data_string distributed_link_tracker_properties file_information io_handle known_folder_location link_target_identifier location_information special_folder_location"
-$TestTypesWithInput = "file"
+$LibraryTests = "data_block data_string distributed_link_tracker_properties error file_information io_handle known_folder_location link_target_identifier location_information notify special_folder_location"
+$LibraryTestsWithInput = "file support"
 
 $TestToolDirectory = "..\msvscpp\Release"
 
-Function TestAPIType
+Function RunTest
 {
 	param( [string]$TestType )
 
-	$TestDescription = "Testing API type: ${TestType}"
-	$TestExecutable = "${TestToolDirectory}\${TestPrefix}_test_${TestType}.exe"
+	$TestDescription = "Testing: ${TestName}"
+	$TestExecutable = "${TestToolDirectory}\${TestPrefix}_test_${TestName}.exe"
 
 	$Output = Invoke-Expression ${TestExecutable}
 	$Result = ${LastExitCode}
@@ -55,9 +55,9 @@ If (-Not (Test-Path ${TestToolDirectory}))
 
 $Result = ${ExitIgnore}
 
-Foreach (${TestType} in ${TestTypes} -split " ")
+Foreach (${TestName} in ${LibraryTests} -split " ")
 {
-	$Result = TestAPIType ${TestType}
+	$Result = RunTest ${TestName}
 
 	If (${Result} -ne ${ExitSuccess})
 	{
@@ -65,9 +65,10 @@ Foreach (${TestType} in ${TestTypes} -split " ")
 	}
 }
 
-Foreach (${TestType} in ${TestTypesWithInput} -split " ")
+Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 {
-	$Result = TestAPIType ${TestType}
+	# TODO: add RunTestWithInput
+	$Result = RunTest ${TestName}
 
 	If (${Result} -ne ${ExitSuccess})
 	{
