53bdd4c160c7d60d9a6560c1f5a1568702207cf2
Joachim Metz <joachim.metz@gmail.com>
2023-01-18 06:36:18 +0100

    Applied updates

--- liblnk/lnk_data_blocks.h
+++ liblnk/lnk_data_blocks.h
@@ -1,7 +1,7 @@
 /*
  * The data blocks definition of a Windows Shortcut File (LNK)
  *
- * Copyright (C) 2009-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
