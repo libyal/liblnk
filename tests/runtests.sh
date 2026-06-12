84dbaf37e2b316346508d940d584dba33068980e
Joachim Metz <joachim.metz@gmail.com>
2026-06-12 23:10:02 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260602
+# Version: 20260609
 
 if test -f ${PWD}/liblnk/.libs/liblnk.1.dylib && test -f ./pylnk/.libs/pylnk.so
 then
 	install_name_tool -change /usr/local/lib/liblnk.1.dylib ${PWD}/liblnk/.libs/liblnk.1.dylib ./pylnk/.libs/pylnk.so
 fi
 
+make check-build > /dev/null
+
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
