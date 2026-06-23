58fb4ee1e78c6062db8afe95c8659250b3e9b7bb
Joachim Metz <joachim.metz@gmail.com>
2026-06-23 08:38:34 +0200

    Worked on tests and changes for CI tests

--- include/liblnk/error.h
+++ include/liblnk/error.h
@@ -82,7 +82,7 @@ enum LIBLNK_ARGUMENT_ERROR
 	 */
 	LIBLNK_ARGUMENT_ERROR_UNSUPPORTED_VALUE		= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBLNK_ARGUMENT_ERROR_CONFLICTING_VALUE		= 9
 };
@@ -214,7 +214,7 @@ enum LIBLNK_OUTPUT_ERROR
 {
 	LIBLNK_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBLNK_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
