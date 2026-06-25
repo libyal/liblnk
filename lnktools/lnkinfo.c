c18035ba89e794a1e553cff2aafe16517d9eb658
Joachim Metz <joachim.metz@gmail.com>
2026-06-25 08:35:50 +0200

    Worked on options and manpages

--- lnktools/lnkinfo.c
+++ lnktools/lnkinfo.c
@@ -1,5 +1,5 @@
 /*
- * Shows information obtained from a Windows Shortcut File (LNK)
+ * Shows information obtained from a Windows Shortcut File (LNK).
  *
  * Copyright (C) 2009-2026, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -21,11 +21,8 @@
 
 #include <common.h>
 #include <file_stream.h>
-#include <memory.h>
 #include <system_string.h>
 #include <types.h>
-
-#include <stdio.h>
 
 #if defined( HAVE_FCNTL_H ) || defined( WINAPI )
 #include <fcntl.h>
@@ -55,31 +52,6 @@
 
 info_handle_t *lnkinfo_info_handle = NULL;
 int lnkinfo_abort                  = 0;
-
-/* Prints the executable usage information
- */
-void usage_fprint(
-      FILE *stream )
-{
-	if( stream == NULL )
-	{
-		return;
-	}
-	fprintf( stream, "Use lnkinfo to determine information about a Windows Shortcut File (LNK).\n\n" );
-
-	fprintf( stream, "Usage: lnkinfo [ -c codepage ] [ -hvV ] source\n\n" );
-
-	fprintf( stream, "\tsource: the source file\n\n" );
-
-	fprintf( stream, "\t-c:     codepage of ASCII strings, options: ascii, windows-874,\n"
-	                 "\t        windows-932, windows-936, windows-949, windows-950,\n"
-	                 "\t        windows-1250, windows-1251, windows-1252 (default),\n"
-	                 "\t        windows-1253, windows-1254, windows-1255, windows-1256\n"
-	                 "\t        windows-1257 or windows-1258\n" );
-	fprintf( stream, "\t-h:     shows this help\n" );
-	fprintf( stream, "\t-v:     verbose output to stderr\n" );
-	fprintf( stream, "\t-V:     print version\n" );
-}
 
 /* Signal handler for lnkinfo
  */
@@ -133,13 +105,26 @@ int wmain( int argc, wchar_t * const argv[] )
 int main( int argc, char * const argv[] )
 #endif
 {
-	liblnk_error_t *error                     = NULL;
-	system_character_t *option_ascii_codepage = NULL;
-	system_character_t *source                = NULL;
-	char *program                             = "lnkinfo";
-	system_integer_t option                   = 0;
-	int result                                = 0;
-	int verbose                               = 0;
+	const char *description = \
+		"Use lnkinfo to determine information about a Windows Shortcut File (LNK).";
+
+	lnktools_option_t options[ ] = {
+		{ 'c', "codepage", "codepage of ASCII strings, options: ascii, windows-874, windows-932, windows-936, windows-949, windows-950, windows-1250, windows-1251, windows-1252 (default), windows-1253, windows-1254, windows-1255, windows-1256, windows-1257 or windows-1258" },
+		{ 'h', NULL, "shows this help" },
+		{ 'v', NULL, "verbose output to stderr" },
+		{ 'V', NULL, "print version" },
+		{ 0, "source", "the source file" },
+	};
+	system_character_t options_string[ 32 ];
+
+	int result                          = 0;
+	liblnk_error_t *error               = NULL;
+	system_character_t *option_codepage = NULL;
+	system_character_t *source          = NULL;
+	char *program                       = "lnkinfo";
+	system_integer_t option             = 0;
+	int number_of_options               = (int) ( sizeof( options ) / sizeof( lnktools_option_t ) );
+	int verbose                         = 0;
 
 #if defined( __MINGW32__ ) && defined( HAVE_MINGW_BINMODE )
 	_setmode( _fileno( stdout ), _O_BINARY );
@@ -176,10 +161,22 @@ int main( int argc, char * const argv[] )
 	 stdout,
 	 program );
 
+	if( lnktools_getopt_get_options_string(
+	     options,
+	     number_of_options,
+	     options_string,
+	     32 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to determine options string.\n" );
+
+		goto on_error;
+	}
 	while( ( option = lnktools_getopt(
 	                   argc,
 	                   argv,
-	                   _SYSTEM_STRING( "c:hvV" ) ) ) != (system_integer_t) -1 )
+	                   options_string ) ) != (system_integer_t) -1 )
 	{
 		switch( option )
 		{
@@ -190,19 +187,27 @@ int main( int argc, char * const argv[] )
 				 "Invalid argument: %" PRIs_SYSTEM "\n",
 				 argv[ optind - 1 ] );
 
-				usage_fprint(
-				 stdout );
+				lnktools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_FAILURE );
 
 			case (system_integer_t) 'c':
-				option_ascii_codepage = optarg;
+				option_codepage = optarg;
 
 				break;
 
 			case (system_integer_t) 'h':
-				usage_fprint(
-				 stdout );
+				lnktools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_SUCCESS );
 
@@ -224,8 +229,12 @@ int main( int argc, char * const argv[] )
 		 stderr,
 		 "Missing source file.\n" );
 
-		usage_fprint(
-		 stdout );
+		lnktools_getopt_usage_fprint(
+		 stdout,
+		 program,
+		 description,
+		 options,
+		 number_of_options );
 
 		return( EXIT_FAILURE );
 	}
@@ -249,11 +258,11 @@ int main( int argc, char * const argv[] )
 
 		goto on_error;
 	}
-	if( option_ascii_codepage != NULL )
+	if( option_codepage != NULL )
 	{
 		result = info_handle_set_ascii_codepage(
 		          lnkinfo_info_handle,
-		          option_ascii_codepage,
+		          option_codepage,
 		          &error );
 
 		if( result == -1 )
@@ -278,8 +287,7 @@ int main( int argc, char * const argv[] )
 	{
 		fprintf(
 		 stderr,
-		 "Unable to open: %" PRIs_SYSTEM ".\n",
-		 source );
+		 "Unable to open source file.\n" );
 
 		goto on_error;
 	}
