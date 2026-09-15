c9a3ea1edf69c08ca098d3adcf5a1208a3f6d616
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 13:28:21 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/liblnk/.libs/liblnk.1.dylib && test -f ./pylnk/.libs/pylnk.so
+if [ -f "${PWD}/liblnk/.libs/liblnk.1.dylib" ] && [ -f ./pylnk/.libs/pylnk.so ]
 then
-	install_name_tool -change /usr/local/lib/liblnk.1.dylib ${PWD}/liblnk/.libs/liblnk.1.dylib ./pylnk/.libs/pylnk.so
+    install_name_tool -change /usr/local/lib/liblnk.1.dylib "${PWD}/liblnk/.libs/liblnk.1.dylib" ./pylnk/.libs/pylnk.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
